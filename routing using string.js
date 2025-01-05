cbcd70d30622d7bfb9f5112e78d696f66986a182
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-01-05 14:08:24 +0530

    edited

--- routing using string.js
+++ routing using string.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app=express();
 //using string path /fd?er then this will only work when the user contains fer or fder or fder
+//above comment should be kept in mind
 app.get("/ab?qe",(req,res)=>{
     res.send("working");
 });
